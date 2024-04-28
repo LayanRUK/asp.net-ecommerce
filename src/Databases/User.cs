7777e6d88ec48ce32daf4c6f68f2f6f86d29ee62
SARAHHAL <sarahabdulrahman61@gmail.com>
2024-04-28 14:57:02 +0300

    Create users obj

--- src/Databases/User.cs
+++ src/Databases/User.cs
@@ -7,6 +7,20 @@ namespace sda_onsite_2_csharp_backend_teamwork.src.Databases
 {
     public class User
     {
-    
+        List<User>? users;
+
+        public User(string id, string name, string email, string password)
+        {
+            users = [
+
+            new User ("1","sarah","sarah@gmail.com ", "sarah1"),
+
+            new User ("2","lama","lama@gmail.com ", "lama2"),
+
+            new User ("3","layan","layan@gmail.com ", "layan3"),
+
+            ];
+
+        }
     }
 }
