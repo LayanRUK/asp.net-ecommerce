c5c0b685e2cf2389046bbac6a84ed9ebcd4dd1c0
Lama782 <lama782@hotmail.com>
2024-05-05 16:14:38 +0300

    changes

---
+++ src/Controllers/OrderController.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using sda_onsite_2_csharp_backend_teamwork.src.Abstractions;
+public class OrderController : BaseController
+{
+    public IOrderService _orderService;
+    public OrderController(IOrderService orderService)
+    {
+        _orderService = orderService;
+    }
+
+    [HttpGet]
+    public IEnumerable<Order> GetAll()
+    {
+        return _orderService.GetAll();
+    }
+    [HttpPost]
+    public IEnumerable<Order> CreateOne(Order order)
+    {
+        return _orderService.CreateOne(order);
+
+
+    }
+
+    [HttpDelete("{id}")]
+    public IEnumerable<Order> DeleteOne(Guid id)
+    {
+        return _orderService.DeleteOne(id);
+    }
+
+    [HttpGet("{id}")]
+    public IEnumerable<Order> FindOne(Guid id)
+    {
+
+        return _orderService.FindOne(id);
+    }
+
+    //   public IEnumerable<Order> DeleteOne(string id){
+    //  _orderService=_orderService.Where(u => u.Id != id);
+    //  return _orderService;
+    // }
+
+}
