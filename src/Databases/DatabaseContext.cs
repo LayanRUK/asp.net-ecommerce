eba20d42ab5c8795ce45f7de026d154ddaaa0739
LayanRUK <LayanAlRukban@gmail.com>
2024-05-06 11:26:30 +0300

    set db and orders

--- src/Databases/DatabaseContext.cs
+++ src/Databases/DatabaseContext.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
 using sda_onsite_2_csharp_backend_teamwork.src.Entities;
 using sda_onsite_2_csharp_backend_teamwork.src.Services;
 
@@ -6,17 +7,17 @@ using sda_onsite_2_csharp_backend_teamwork.src.Services;
 namespace sda_onsite_2_csharp_backend_teamwork.src.Databases
 {
 
-    public class DatabaseContext
+    public class DatabaseContext : DbContext
 
     {
-        public IEnumerable<Order> orders { get; set; }
+        public DbSet<Order> orders { get; set; }
+        public DbSet<Order_Item> orderItems { get; set; }
         public IEnumerable<Product> products;
         public IEnumerable<User> users { get; set; }
+
+        private IConfiguration _config;
         public DatabaseContext()
         {
-            orders = [
-            new Order("1","123","132",0506636619,"67"),
-        ];
 
             products = new List<Product>
         {
@@ -34,6 +35,17 @@ namespace sda_onsite_2_csharp_backend_teamwork.src.Databases
             ];
 
 
+        }
+        public DatabaseContext(IConfiguration config)
+        {
+            _config = config;
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseNpgsql(@$"Host={_config["Db:Host"]};Username={_config["Db:Username"]};Database={_config["Db:Database"]};Password={_config["Db:Password"]}")
+           .UseSnakeCaseNamingConvention();
         }
 
     }
