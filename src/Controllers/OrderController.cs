cc6a983f5ff7d03ae39474b43952a67445d622c2
Lama782 <lama782@hotmail.com>
2024-05-08 16:26:39 +0300

    protect order

--- src/Controllers/OrderController.cs
+++ src/Controllers/OrderController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Controllers;
 using sda_onsite_2_csharp_backend_teamwork.src.Abstractions;
@@ -18,26 +19,7 @@ public class OrderController : BaseController
     {
         return _orderService.GetAll();
     }
-    [HttpPost]
-     [ProducesResponseType(StatusCodes.Status201Created)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-
-    public ActionResult <Order> CreateOne([FromBody] OrderCreatDto order)
-    {
-        
-      if (order is not null)
-      {
-        var createdorder = _orderService.CreateOne(order);
-        return CreatedAtAction(nameof(CreateOne), order);
-      }
-      return BadRequest();
-
-    }
-    // [HttpDelete("{id}")]
-    // public IEnumerable<Order> DeleteOne(Guid id)
-    // {
-    //     return _orderService.DeleteOne(id);
-    // }
+ 
     [HttpGet("{id}")]
     public OrderReadDto FindOne(Guid id)
     {
@@ -45,8 +27,8 @@ public class OrderController : BaseController
         return _orderService.FindOne(id);
     }
 
+    [Authorize(Roles = "Admin,Customer")]
     [HttpPost("checkout")]
-
     public Order Checkout(List<OrderItemCreateDto> orderItemCreateDtos)
     {
         return _orderService.Checkout(orderItemCreateDtos);
